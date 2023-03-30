1be861159fe32674b01e181448f78ac38616aa35
Cristian Paniagua <tenonde@MacBook-Pro-M1-Pro.local>
2023-03-30 14:17:53 -0400

    0.05

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!. Version 0.0.4",
+        message: "Go Serverless v3.0! Your function executed successfully!. Version 0.0.5",
         input: event,
       },
       null,
